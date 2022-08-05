417d3fbf1c3e01143a912d0dac44cfcaa8c6fdf1
Fernando Nicolás <fersanti2896@gmail.com>
2022-08-05 17:29:33 -0500

    Generar y Visualizar PDf en GeneradorPDFController.cs

--- wwwroot/js/FuncionesGenerales.js
+++ wwwroot/js/FuncionesGenerales.js
@@ -42,3 +42,72 @@ function SetupDescarga() {
         })
     })
 }
+
+function GetModeloPDF() {
+    var modelo = {
+        html: $("#panel").html()
+    }
+    return modelo;
+}
+
+function GenerarPDF() {
+    var namePDF = $("#panel").data("prefix");
+    $.ajax({
+        type: 'POST',
+        url: "/GeneradorPDF/GenerarPDF",
+        data: GetModeloPDF(),
+        async: false,
+        cache: false,
+        success: function (resultado) {
+            if (resultado.success) {
+                window.location = "/GeneradorPDF/DownloadPDF?namePDF=" + namePDF;
+            }
+        },
+        error: function (errorHtml) {
+            alert(controlador + ":" + JSON.stringify(errorHtml));
+        },
+        complete: function () {
+
+        }
+    });
+}
+
+function GenerarVisorPDF() {
+    $.ajax({
+        type: 'POST',
+        url: "/GeneradorPDF/GeneradorVisorPDF",
+        data: GetModeloPDF(),
+        dataType: "json",
+        async: false,
+        cache: false,
+        success: function (resultado) {
+            if (resultado.success) {
+                var arrayBuffer = base64ToArrayBuffer(resultado.content);
+
+                function base64ToArrayBuffer(base64) {
+                    var binaryString = window.atob(base64);
+                    var binaryLen = binaryString.length;
+                    var bytes = new Uint8Array(binaryLen);
+
+                    for (var i = 0; i < binaryLen; i++) {
+                        var ascii = binaryString.charCodeAt(i);
+                        bytes[i] = ascii;
+                    }
+
+                    return bytes;
+                }
+
+                var blob = new Blob([arrayBuffer], { type: "application/pdf" });
+                var link = window.URL.createObjectURL(blob);
+
+                window.open(link, '', 'height=650,width=840');
+            }
+        },
+        error: function (errorHtml) {
+            alert(controlador + ":" + JSON.stringify(errorHtml));
+        },
+        complete: function () {
+
+        }
+    });
+}
